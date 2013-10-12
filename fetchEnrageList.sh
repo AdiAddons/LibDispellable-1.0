436f989a5d1cf107b4eb6fe97cf8e122632dc548
Adirelle <adirelle@gmail.com>
2013-10-12 11:57:02 +0200

    Updated for 5.4.

--- fetchEnrageList.sh
+++ fetchEnrageList.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Get the list of spell IDs with dispel type "Enrage" from db.mmo-champion.com
-curl -s http://db.mmo-champion.com/spells/?dispel_type=9 | perl -ne '/.id.: (\d+)/ and print "$1,\n";' | sort -nu | indent -nhnl
-
+curl -s http://www.wowdb.com/spells?filter-dispel-type=9 | perl -ne 'm@http://www.wowdb.com/spells/(\d+)\-@ and print "$1,\n";' | sort -nu
